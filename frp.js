a9d28142ee67eb9ec8ae141c311e9d953fe55670
Jay Zawrotny <jayzawrotny@gmail.com>
2016-11-06 11:42:06 -0500

    Refactored frp to be cleaner, simpler code

--- frp.js
+++ frp.js
@@ -1,37 +1,45 @@
+// Functional Reactive Programming with Highland
 let _ = require('highland');
 let format = require('util').format;
 
 const COLORS = [
-  { stop: 0, rgb: [245, 51, 0] }, // red
+  { stop: 0, rgb: [44, 163, 178] }, // turk-like colors
   { stop: 6, rgb: [248, 235, 73] }, // yellow
-  { stop: 48, rgb: [44, 163, 178] }, // turk-like colors
+  { stop: 48, rgb: [245, 51, 0] }, // red
 ];
 
 function calcGradient (hours) {
-  return _(COLORS).reduce({ done: false, colors: [] }, (loop, color) => {
-    !loop.done && loop.colors.push(color);
-    return { done: color.stop > hours, colors: loop.colors.slice(-2) };
-  })
-  .pluck('colors')
-  .flatMap(([c1, c2]) => _(c1.rgb).zip(c2.rgb).map(([x1, x2]) => [
-    x1, x2, Math.max((hours - c1.stop) / (c2.stop - c1.stop), 0),
-  ]))
-  .map(([c1, c2, factor]) => Math.floor(c1 + ((c2 - c1) * factor))).collect();
+  let done = false;
+
+  return _(COLORS)
+    .reduce([], (colors, color) => {
+      !done && colors.push(color);
+      done = color.stop > hours;
+      return colors.slice(-2);
+    })
+    .map(([end, start]) => ({
+      rgb: [start.rgb, end.rgb],
+      factor: Math.max((hours - start.stop) / (end.stop - start.stop), 0),
+    }))
+    .flatMap(({ rgb, factor }) => {
+      return _(rgb[0]).zip(rgb[1]).map((values) => ({ values, factor }));
+    })
+    .map(({ values, factor }) => {
+      return Math.floor(values[0] + (values[1] - values[0]) * factor);
+    })
+    .collect();
 }
 
 // Timer logic
 // Pretty shitty but just playing around
-function simulateTime () {
-  let hours = 0;
-  let endTime = 50;
-
+function simulateTime (hours=0) {
   _((push, next) => {
     push(null, hours);
-    if (hours <= endTime) setTimeout(next, 1000);
-    hours += 2;
+    hours <= 50 && setTimeout(next, 1000);
   })
   .flatMap(calcGradient) // pass it to calc gradient
   .map((gradient) => `${format(gradient)} – ${hours} hours after lead\n`)
+  .tap(() => hours += 2)
   .pipe(process.stdout)
 }
 
